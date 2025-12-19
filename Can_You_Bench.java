9a348d77210932e44533f1f1caa3483761b468b4
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-12-20 00:07:49 +0530

    first commit

---
+++ Can_You_Bench.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+ public class Can_You_Bench {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = n + (2* 10);
+
+        System.out.println(result);
+
+
+
+
+
+       
+        sc.close();
+    }
+}
